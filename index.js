7bcde003141ed50915737cd9dbb1b35114d8a468
Dave Beck <dbeck2@outlook.com>
2018-06-18 20:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,25 +5,16 @@ function takeANumber(katzDeli, name) {
   return ('Welcome, ${name}. You are number ${katzDeli.length} in line.');
 }
 
-function nowServing(katzDeliLine) {
- if (katzDeli.length === 0) {
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
    message = 'There is nobody waiting in line!';
   }
  else {
-   message = 'Currently serving ${katzDeliLine.shift()}.';
+   message = 'Currently serving ${katzDeli.shift()}.';
  }
  return message;
 }
-/*
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    mesage = 'The line is currently: '
-    for (let i = 1, i < katzDeliLine.length; i++) {
-      message += i + '. ' + katzDeliLine[i - 1] + ', ';
-    }
-      message += i + '. ' + katzDeliLine[katzDeliLine.length-1];
-      return message;
-  }
-  else return (message = 'The line is currently empty.')
-}
-*/

1209aa1d5a4f1e6080c788dbf5e5a350d2407fb0
Dave Beck <dbeck2@outlook.com>
2018-06-18 19:10:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,16 @@ function nowServing(katzDeliLine) {
  }
  return message;
 }
+/*
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     mesage = 'The line is currently: '
     for (let i = 1, i < katzDeliLine.length; i++) {
-    message += i + '. ' + katzDeliLine[i - 1] + ', ';
+      message += i + '. ' + katzDeliLine[i - 1] + ', ';
     }
-      message += i + '.' + katzDeliLine[katzDeliLine.length-1];
+      message += i + '. ' + katzDeliLine[katzDeliLine.length-1];
       return message;
   }
   else return (message = 'The line is currently empty.')
 }
+*/

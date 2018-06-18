067979dbbee884219082e3485973844afb346a67
Dave Beck <dbeck2@outlook.com>
2018-06-18 21:11:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 var katzDeli = [];
 
-function currentLine(line){
-  if(!line.length) {
-    return "The line is currently empty.";
-  }
-  var lineNamesandNumbers = [];
-
-  for(var i=0; i<line.length; i++) {
-    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
-  }
-  console.log("The line is currently: " + lineNamesandNumbers)
-  return "The line is currently: " + lineNamesandNumbers.join(', ');
+function takeANumber(katzDeli, name) {
+  katzDeli.push('${name}');
+  return ('Welcome, ${name}. You are number ${katzDeli.length} in line.');
 }
 
-function nowServing(line) {
-  if(!line.length) {
-    console.log("There is nobody waiting to be served!")
-    return "There is nobody waiting to be served!"
-  } else {
-    //console.log("Currently serving " + line.shift());
-    return "Currently serving " + line.shift();
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
   }
-}
-
-function takeANumber(line, name){
-  line.push(name);
-
-  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
-
-  return "Welcome, " + name + ". You are number " + line.length + " in line."
-}
+  if (katzDeli.length === 0) {
+   return "There is nobody waiting to be served!";
+  }
+ else
+  return ('Currently serving ${katzDeli.shift()}.');
+ }
 takeANumber(katzDeli, "Ada")
 takeANumber(katzDeli, "Grace")
 takeANumber(katzDeli, "Kent")
